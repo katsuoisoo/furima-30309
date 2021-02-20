de3e936a11fcb976bdd265e8ee5df5d4a6f75f16
katsuoisoo <hiramekatsuo@gmail.com>
2021-02-20 16:27:06 +0900

    ユーザー管理機能：テストコードの再修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -91,6 +91,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include("Email has already been taken")
       end
+      it 'メールアドレスは、@を含まないと登録できない' do
+        @user.email = 'hoge.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
       it 'last_nameが全角（漢字・ひらがな・カタカナ）でないと登録できない' do
         @user.last_name = 'aaa'
         @user.valid?
